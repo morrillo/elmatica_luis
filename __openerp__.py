cd6e8454bf7b8135b915d7408de182c1ad429d9d
morrillo <gustavo.orrillo@moldeo.coop>
2016-05-20 14:22:35 -0300

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Elmatica - Luis module',
+    'category': 'Sales',
+    'version': '0.1',
+    'depends': ['elmatica_sales_purchase'],
+    'data': [
+	'luis_view.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    # 'css': ['static/src/css/styles.css',],
+    'installable': True,
+}
